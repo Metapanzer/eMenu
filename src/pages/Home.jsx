680f214800694a969aea5353cc2869156fcbf539
Krisna Sandy Pribadi <baltheon@gmail.com>
2023-07-31 22:43:51 +0700

    update Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Home() {
   return (
-    <div className="min-h-screen max-w-screen-sm bg-yellow-300 flex flex-col justify-around items-center">
+    <div className="min-h-screen w-screen bg-yellow-300 flex flex-col justify-around items-center border rounded-lg">
       <h1>eMenu</h1>
       <h6>Paperless Menu</h6>
     </div>
