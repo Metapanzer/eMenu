e6fea090a81125036611e6e8a94cdaaefdac8d1f
Krisna Sandy Pribadi <baltheon@gmail.com>
2023-07-31 21:39:49 +0700

    create base Home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+export default function Home() {
+  return (
+    <motion.div
+      className="max-"
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{
+        duration: 0.8,
+        delay: 0.5,
+        ease: [0, 0.71, 0.2, 1.01],
+      }}
+    />
+  );
+}
